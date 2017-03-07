9bf7aaef1c914d78dc73458a4cd82c42cfba7920
Jessica Jin <jessicajin@Jessicas-MacBook-Pro.local>
2017-03-06 16:35:56 -0800

    second commit

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ quizArray = [
 		answer: "cascading style sheets"
 	},
 	{
-		question: "Where does a programmer usually spend 60% of their time?",
+		question: "How do programmers usually spend the majority of their time?",
 		answer: "debugging"
 	},
 	{
-		question: "With dedication, persistence, and hardwork, can anyone become a programming jedi?",
+		question: "With dedication, persistence, and hardwork, can you become a programming jedi?",
 		answer: "yes"
 	}	]
 
@@ -36,42 +36,27 @@ for (var i=0; i<quizArray.length; i++) {
 }
 
 // Function that checks if the inputted answers are correct or incorrect (& counts the total correct & incorrect)
-// first, variables holding # of correct & incorrect answers
 var numCorrect = 0 
 var numIncorrect = 0
 function checkAnswers() {
 	// for (every question)
 	for (var i = 0; i < quizArray.length; i++) {
 		if (document.getElementById("guess"+i).value.toLowerCase() == quizArray[i].answer) {
-		console.log("yay, correct - it works")
-		// marks inputted answer as correct
-		var correct = document.getElementById("guess"+i).value.toLowerCase()
-		console.log(correct)
-		// adds a classname (to turn it green)
-		correct.className = "correct"
-		// add it to the total CORRECT count
-		numCorrect = numCorrect + 1
-		console.log(numCorrect)
+			// add a classname (to turn it green)
+			$('#question'+i).addClass('turngreen');
+			// add it to the total CORRECT count
+			numCorrect = numCorrect + 1
 		} else {
-		console.log("incorrect! but at least it works")
-		// mark inputted answer as incorrect
-		var incorrect = document.getElementById("guess"+i).value.toLowerCase()
-		console.log(incorrect)
-		// adds classname (to turn it red)
-		incorrect.className = "incorrect"
-		// add it to the total INCORRECT count
-		numIncorrect = numIncorrect + 1
-		console.log(numIncorrect)
+			// add classname to the question (to turn it red)
+			$('#question'+i).addClass('turnred');
+			// add it to the total INCORRECT count
+			numIncorrect = numIncorrect + 1	
 		}
 	}
+		// push the total # correct & incorrect onto the HTML page
+		document.getElementById('numCor').innerHTML = numCorrect
+		document.getElementById('numInc').innerHTML = numIncorrect
 }
-
-// good test code that shows this if statement condition works! 
-	// if (document.getElementById("guess0").value.toLowerCase() == quizArray[0].answer) {
-	// 	console.log("yay! this is correct")
-	// } else {
-	// 	console.log("no, this isn't working")
-	// }
 
 
 
